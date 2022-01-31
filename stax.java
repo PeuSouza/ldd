6be1dd8c6a15b20e9495db94dff10cdd8a0e8b56
Ana Paula Souza Silva <78571318+PeuSouza@users.noreply.github.com>
2022-01-31 11:48:01 -0300

    Update stax.java

--- stax.java
+++ stax.java
@@ -1,143 +1,135 @@
-public class stax {
-
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-import javax.xml.stream.*;
-
-import org.xml.sax.Attributes;
-import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
-
-    public class Main extends DefaultHandler {
-        public static void main(String[] args) throws IOException, XMLStreamException {
-            XMLOutputFactory xmlof = XMLOutputFactory.newFactory();
-            Writer writer = new FileWriter("LDD2/STAX.html");
-            XMLStreamWriter xmlsw = (xmlof.createXMLStreamWriter(writer));
-
-            XMLInputFactory xmlif = XMLInputFactory.newFactory();
-
-            Reader reader;
-            reader = new FileReader("products.xml");
-            XMLStreamReader xmlsr = xmlif.createXMLStreamReader(reader);
-
-            List<String> vendor = new ArrayList();
-
-            List<String> name = new ArrayList();
-            List<String> buy = new ArrayList();
-            List<String> msrp = new ArrayList();
-            List<String> percent = new ArrayList();
+import javax.xml.stream.*;
+import java.io.*;
+import java.util.List;
+import java.util.ArrayList;
+public class STAX extends DefaultHandler{
 
 
-            List<String> name2 = new ArrayList();
-            List<String> percentx = new ArrayList();
-            List<String> ppercenty = new ArrayList();
+       public static void main(String[] args) throws IOException, XMLStreamException {
+           XMLOutputFactory xmlof = XMLOutputFactory.newFactory();
+           Writer writer = new FileWriter("saidab.html");
+           XMLStreamWriter xmlsw = (xmlof.createXMLStreamWriter(writer));
 
-            List<String> ivendor = new ArrayList();
+           XMLInputFactory xmlif = XMLInputFactory.newFactory();
 
-            boolean qvendor = false;
-            boolean qname = false;
-            boolean qbuy = false;
-            boolean qmsrp= false;
+           // leitor
+           Reader reader;
+           reader = new FileReader("products.xml");
+           XMLStreamReader xmlsr = xmlif.createXMLStreamReader(reader);
 
-            String variavel = null;
+           List<String> vendor = new ArrayList();
 
-            while (xmlsr.hasNext()) {
-                switch (xmlsr.next()) {
-                   case XMLStreamReader.startvariavel:
-                      variavel = xmlsr.getLocalName();
-
-                 if (variavel.equals("name"))
-                   {
-                     qnome = true;
-                   }
-
-                 if (variavel.equals("buyPrice"))
-                   {
-                     qbuy = true;
-                   }
-
-                 if (variavel.equals("MSRP"))
-                   {
-                     qmsrp = true;
-                   }
-                   break;
-
-                    case XMLStreamReader.endvariavel:
-                        if (xmlsr.getLocalName().equals("product")) {
-                        }
-                        break;
-
-                    case XMLStreamReader.characters:
-
-                        if (qnome == true) {
-                            name.add(xmlsr.getText());
-                        }
-                            qname = false;
-                        break;
-
-                        if (qbuy == true) {
-                            buy.add(xmlsr.getText());
-                        }
-                            qbuy = false;
-                        break;
-
-                        if (qmsrp == true) {
-                            msrp.add(xmlsr.getText());
-                        }
-                            qmsrp = false;
-                        break;
-                }
-            }
+           List<String> name = new ArrayList();
+           List<String> buy = new ArrayList();
+           List<String> msrp = new ArrayList();
+           List<String> percent = new ArrayList();
 
 
-            for(int a = 0;a < nomes.size();a++){
-                double valorx = Double.parseDouble(buys.get(a));
-                double valory = Double.parseDouble(msrp.get(a));
+           List<String> name2 = new ArrayList();
+           List<String> percentx = new ArrayList();
+           List<String> percenty = new ArrayList();
+
+           List<String> ivendor = new ArrayList();
+
+           boolean qvendor = false;
+           boolean qname = false;
+           boolean qbuy = false;
+           boolean qmsrp = false;
+
+           String element = null;
+
+           while (xmlsr.hasNext()) {
+               switch (xmlsr.next()) {
+                   case XMLStreamReader.START_ELEMENT:
+
+                       element = xmlsr.getLocalName();
+
+                       if (element.equals("name")) {
+                           qname = true;
+                       }
+
+                       if (element.equals("buyPrice")) {
+                           qbuy = true;
+                       }
+
+                       if (element.equals("MSRP")) {
+                           qmsrp = true;
+                       }
+                       break;
+
+                   case XMLStreamReader.END_ELEMENT:
+
+                       if (xmlsr.getLocalName().equals("product")) {
+                       }
+                       break;
+
+                   case XMLStreamReader.CHARACTERS:
+
+                       if (qname == true) {
+                           name.add(xmlsr.getText());
+                           qname = false;
+                       }
+
+                       if (qbuy == true) {
+                           buy.add(xmlsr.getText());
+                           qbuy = false;
+                       }
 
 
-                double percenty = (valorx * 100)/  valory;
+                       if (qmsrp == true) {
+                           msrp.add(xmlsr.getText());
+                           qmsrp = false;
+                       }
+                       break;
 
-                percentx.add(String.valueOf((int) Math.round(percenty)));
-            }
+               }
+           }
 
-            System.out.println(percent);
-
-            for (int k = 0; k < percent.size();k++)
-            {
-                percentx.add(porcentagem.get(k));
-            }
-
-            percent.sort((o1, o2) -> (int) Math.round(Float.parseFloat(o2)) - (int) Math.round(Float.parseFloat(o1)));
+           for (int a = 0; a < name.size(); a++) {
+               double valorx = Double.parseDouble(buy.get(a));
+               double valory = Double.parseDouble(msrp.get(a));
 
 
-            XMLOutputFactory xmlof2 = XMLOutputFactory.newFactory();
-            Writer writer2 = new FileWriter("STAX.html");
+               double calculo = (valorx * 100) / valory;
 
-            xmlsw.writeStartDocument();
+               percent.add(String.valueOf((int) Math.round(calculo)));
 
-            xmlsw.writeStartElement("ol");
-            for (int q = 0; q < percent.size();q++)
-            {
-                for (int w = 0; w < percent.size();w++)
-                {
-                    if (percent.get(q).contains(pescentx.get(w)))
-                        if (!name2.contains(name.get(w))){
-                            name2.add(name.get(w));
-                            xmlsw.writeStartElement("li");
-                            xmlsw.writeCharacters(name.get(w) + " ("+ percent.get(q)+ ")");
-                            xmlsw.writeEndElement();
-                        }
-                }
-            }
+           }
 
+
+           for (int i = 0; i < percent.size(); i++) {
+               percentx.add(percent.get(i));
+           }
+
+           percent.sort((o1, o2) -> (int) Math.round(Float.parseFloat(o2)) - (int) Math.round(Float.parseFloat(o1)));
+
+           XMLOutputFactory xmlof2 = XMLOutputFactory.newFactory();
+
+           xmlsw.writeStartDocument();
+
+           xmlsw.writeStartElement("ol");
+           for (int e = 0; e < percent.size(); e++) {
+               for (int p = 0; p < percent.size(); p++) {
+                   if (percent.get(e).contains(percentx.get(p))){
+                       if (!name2.contains(name.get(p))) {
+                           name2.add(name.get(p));
+                           xmlsw.writeStartElement("li");
+                           xmlsw.writeCharacters(name.get(p) + " (" + percent.get(e) + ")");
+                           xmlsw.writeEndElement();
+                       }
+               }
+           }
+
+       }
             xmlsw.writeEndElement();
             xmlsw.writeEndDocument();
             xmlsw.close();
-       }
+        }
     }
-}
+
+
+
+
+
+
